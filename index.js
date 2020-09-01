a3a553ad22c4aa334d49d3ca2ee7dd73440cc757
sdowns716 <66967392+sdowns716@users.noreply.github.com>
2020-09-01 13:25:17 -0400

    Added business logic

--- index.js
+++ index.js
@@ -1,214 +1,218 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
  morgan = require('morgan'),
  app = express();
  bodyParser = require('body-parser');
-
+ uuid = require("uuid");
 app.use(bodyParser.json());
 
 
-let movies = [
-  {
-    title: 'Bambi',
-    description: 'A fawn who will one day become King of the forest is born.  He befriends different animals who help him navigate phases of his life.',
-    genre:{
-      genre: 'Animated',
-      description: 'Cartoon with moving pictures'
-    },
-    director: {
-      name: 'David Hand',
-      bio: 'Born in Plainfield, New Jersey, Hand began his animation career working on the Out of the Inkwell cartoons throughout the 1920s. He joined the Disney studio in 1930.  Hand contributed to Midnight in a Toy Shop, The China Plate and Egyptian Melodies.',
-      birthYear: 1900,
-      deathYear: 1996
-    },
-    imageURL: 'bambi.png',
-    featured: 'No',
-  },
-  {
-    title: 'Peter Pan',
-    description: 'A free-spirited and mischevious young boy who never grows up spends his time on the island of Neverland as the leader of the Lost Boys.',
-    genre:{
-      genre: 'Animated',
-      description: 'Cartoon with moving pictures'
-    },
-    director: {
-      name: 'Clyde Geronimi',
-      bio: 'Born in Italy, he immigrated to the US as a young child.  His earliest experience in animation was through J.R. Bray Studios and joined Disney in 1931.',
-      birthYear: 1901,
-      deathYear: 1989
-    },
-    imageURL: 'peterpan.png',
-    featured: 'Yes',
-  }
-
-/*  {
-    title: 'Sleeping Beauty',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 8,
-      deathYear: 9
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'The Parent Trap',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 9999,
-      deathYear: 8888
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'Mary Poppins',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 8888,
-      deathYear: 9999
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'Jungle Book',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 8888,
-      deathYear: 9999
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'Robin Hood',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 7,
-      deathYear: 9,
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'The Rescuers',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 7,
-      deathYear: 9
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'Honey, I Shrunk the Kids',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 1999,
-      deathYear: 47777,
-    },
-    imageURL: '',
-    featured: '',
-  },
-  {
-    title: 'The Little Mermaid',
-    description: '',
-    genre:'',
-    director: {
-      name: '',
-      bio: '',
-      birthYear: 1900,
-      deathYear: 1900,
-    },
-    imageURL: '',
-    feature: '', */
-];
-
 app.use(express.static('public'));
 app.use(morgan('common'));
-
 //Error handling middleware
 app.use((err, _req, res, _next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-
 // GET requests
 app.get('/', (_req, res) => {
   res.send('Welcome to my Disney Movie App!');
 });
-
 app.get('/documentation', (_req, res) => {
   res.sendFile('documentation.html', { root: __dirname });
 });
 
+// Get all movies
 app.get('/movies', (_req, res) => {
-  res.json(movies);
-});
+  Movies.find()
+    .then(function(movies) {
+      res.status(201).json(movies)
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+  });
 
 // Gets all of the data about a single movie, by title
-app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movie) =>
-    { return movie.title === req.params.title }));
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title : req.params.Title })
+  .then((movie) => {
+    res.json(movie)
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+//Gets genre details
+app.get('/movies/genres/:Name', (req, res) => {
+  Movies.findOne({'Genre.Name': req.params.Name})
+  .then(function(movie){
+    res.json(movie)
+    })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error:" + err);
+  });
 });
 
 //Gets data about a genre of single movie, by title
-app.get('/movies/:title/genre', (_req, res) => {
-  res.send('Successful GET request returning data about the genre of the movie');
+app.get('/movies/genres/:Title', (req, res) => {
+  Movies.findOne({'Genre.Title': req.params.Title})
+  .then(function(movie){
+    if(movie){
+      res.status(201).send("Movie with the title : " + movie.Title + " is  a " + movie.Genre.Name + " ." );
+    }else{
+      res.status(404).send("Movie with the title : " + req.params.Title + " was not found.");
+        }
+    })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error:" + err);
+  });
 });
 
 //Gets data about a director (bio, birth year & death year by name)
-app.get('/movies/:title/directors', (_req, res) => {
-    res.send('Successful GET request returning data (bio, birth year & death) about the director');
+app.get('/movies/directors/:Name', (req, res) => {
+  Movies.findOne({"Director.Name" : req.params.Name})
+  .then(function(movies){
+    res.json(movies.Director)
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.status(500).send("Error:" + err);
   });
+});
 
   // Adds data for a new user registration
-app.post('/users', (_req, res) => {
-    res.send('Successful POST request adding data for a new user');
+  app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
+
+  // Get all users
+app.get('/users', (_req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //Allows users to update their info(username, password, email, DOB)
-app.put('/users/:id', (_req, res) => {
-  res.send('Successful PUT request allowing users to update their info (username, password, email, DOB)');
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-  //Allow users to add movie to favorites
-  app.post('/users/:id/favorites/:title', (_req, res) => {
-    res.send ('Successful POST request allowing users to add a movie to their favorites')
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-  //Allow users to remove movie from favorites
-app.delete('/users/:id/favorites/:title', (_req, res) => {
-  res.send ('Successful DELETE request allowing users to remove a movie from their favorites')
+//Delete movie by username
+app.delete('/users/:Username/Movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username : req.params.Username }, {
+    $pull : { FavoriteMovies : req.params.MovieID }
+  },
+  { new : true }, // This line makes sure that the updated document is returned
+  function(err, updatedUser) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    } else {
+      res.json(updatedUser)
+    }
+  })
 });
 
-  //Allow users to deregister by ID
-app.delete('/users/:id', (_req, res) => {
-  res.send ('Successful DELETE request allowing users to deregister with their ID')
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 // listen for requests
 app.listen(8080, () =>
-  console.log('Your app is listening on port 8080.'));
+console.log('Your app is listening on port 8080.'));
