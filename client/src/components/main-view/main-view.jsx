fe4ab03cae5e18649555aaaa993e9e0464f4910d
sdowns716 <66967392+sdowns716@users.noreply.github.com>
2020-09-23 15:14:24 -0400

    updated main-view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
 import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-    constructor() {
-      super();
-  
-      this.state = {
-        movies: null,
-        selectedMovie: null,
-        user: null,
-      };
-    }
 
-  // One of the "hooks" available in a React Component
+  constructor() {
+    super();
+
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
+  }
+
   componentDidMount() {
-    axios.get('/https://sydney-flix-app.herokuapp.com/movies')
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    /* ... */
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
   }
 
 
   render() {
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
 
     return (
      <div className="main-view">
-     { movies.map(movie => (
-       <div className="movie-card" key={movie._id}>{movie.Title}</div>
-     ))}
+      {selectedMovie
+         ? <MovieView movie={selectedMovie}/>
+         : movies.map(movie => (
+           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+         ))
+      }
      </div>
     );
   }
--- client/src/components/movie-view.jsx
+++ client/src/components/movie-view.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export class MovieView extends React.Component {
-
   constructor() {
     super();
 
