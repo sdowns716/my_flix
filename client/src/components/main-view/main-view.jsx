22e235a97694ff3db49edab30d3e210366d2b7f5
sdowns716 <66967392+sdowns716@users.noreply.github.com>
2020-09-24 13:19:33 -0400

    added movie loop code

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -5,52 +5,34 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      movies: null,
-      selectedMovie: null
-    };
-  }
-
-  componentDidMount() {
-      axios
-        .get("https://sydney-flix-app.herokuapp.com/movies")
-        .then((response) => {
-          // Assign the result to the state
-          this.setState({
-            movies: response.data,
-          });
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-  }
-
-  onMovieClick(movie) {
-    this.setState({
-      selectedMovie: movie
+// One of the "hooks" available in a React Component
+componentDidMount() {
+  axios.get('https://sydney-flix-app.herokuapp.com//movies')
+    .then(response => {
+      // Assign the result to the state
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-  }
+}
 
+render() {
+  // If the state isn't initialized, this will throw on runtime
+  // before the data is initially loaded
+  const { movies } = this.state;
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+  // Before the movies have been loaded
+  if (!movies) return <div className="main-view"/>;
 
-    // Before the movies have been loaded
-    if (!movies) return <div className="main-view"/>;
-
-    return (
-     <div className="main-view">
-      {selectedMovie
-         ? <MovieView movie={selectedMovie}/>
-         : movies.map(movie => (
-           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-         ))
-      }
-     </div>
-    );
-  }
+  return (
+   <div className="main-view">
+   { movies.map(movie => (
+     <div className="movie-card" key={movie._id}>{movie.Title}</div>
+   ))}
+   </div>
+  );
+}
 }
